c6356161fce2eb87bc9d854fab9178881df62768
Matthew Salerno <matthewsalern@gmail.com>
2013-09-23 20:41:13 -0400

    second model spec passing

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,11 @@ class Quote
 
   attr_accessor :author, :text
 
+  def self.from_json(json)
+    new.tap do |quote|
+      quote.author = json["contents"]["author"]
+      quote.text = json["contents"]["quote"]
+    end
+  end
+
 end
--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -1,9 +1,33 @@
 describe "Quote" do
 
+  before do
+    @json_quote = {
+      "contents"=> {
+        "quote"=> "I wanted to beat the heck out of the JV guys for that, except I wouldn't know what to do in a fistfight without a manual. ,",
+        "id"=> "5krzvfoA9GSEeNnUVmLVQQeF",
+        "author"=> "Carrie Harris",
+        "length"=> "123",
+        "tags"=> [
+          "fighting",
+          "football",
+          "funny",
+          "manual",
+          "nerd"
+        ]
+      }
+    }
+  end
+
   it "should have an author and text" do
-    quote = Quote.new.tap {|q| q.author = "Henry Miller"; q.text = "I'm a great author and very smart"}
+    quote = Quote.new.tap { |q| q.author = "Henry Miller"; q.text = "I'm a great author and very smart" }
     quote.author.should == "Henry Miller"
     quote.text.should == "I'm a great author and very smart"
+  end
+
+  it "should instantiate a quote from json" do
+    quote = Quote.from_json(@json_quote)
+    quote.author.should == "Carrie Harris"
+    quote.text.should == "I wanted to beat the heck out of the JV guys for that, except I wouldn't know what to do in a fistfight without a manual. ,"
   end
 
 
