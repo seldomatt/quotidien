64764644399391c223ae950a1dd70b19abea6fcd
Matthew Salerno <matthewsalern@gmail.com>
2013-09-23 20:58:31 -0400

    functional test without stubbing

--- spec/controllers/find_quote_controller_spec.rb
+++ spec/controllers/find_quote_controller_spec.rb
@@ -6,30 +6,34 @@ describe "FindQuoteController" do
     view("Quote Generator Button").should != nil
   end
 
-  before do
-    @json_quote = {
-      "contents"=> {
-        "quote"=> "I wanted to beat the heck out of the JV guys for that, except I wouldn't know what to do in a fistfight without a manual. ,",
-        "id"=> "5krzvfoA9GSEeNnUVmLVQQeF",
-        "author"=> "Carrie Harris",
-        "length"=> "123",
-        "tags"=> [
-          "fighting",
-          "football",
-          "funny",
-          "manual",
-          "nerd"
-        ]
-      }
-    }
-    controller.should.receive(:get_random_quote_from_api).and_yield(@json_quote)
-  end
+  describe "#set_quote" do
 
-  it "should fetch a random quote when quote generator is pressed" do
-    tap("Quote Generator Button")
-    controller.quote.class.should == Quote
-    controller.quote.author.should == "Carrie Harris"
-    controller.quote.text.should == "I wanted to beat the heck out of the JV guys for that, except I wouldn't know what to do in a fistfight without a manual. ,"
+    before do
+      @json_quote = {
+        "contents"=> {
+          "quote"=> "I wanted to beat the heck out of the JV guys for that, except I wouldn't know what to do in a fistfight without a manual. ,",
+          "id"=> "5krzvfoA9GSEeNnUVmLVQQeF",
+          "author"=> "Carrie Harris",
+          "length"=> "123",
+          "tags"=> [
+            "fighting",
+            "football",
+            "funny",
+            "manual",
+            "nerd"
+          ]
+        }
+      }
+      #controller.should.receive(:get_random_quote_from_api).and_yield(@json_quote)
+    end
+
+    it "should take parsed json quote from api call and set attribute" do
+      controller.set_quote(@json_quote)
+      controller.quote.class.should == Quote
+      controller.quote.author.should == "Carrie Harris"
+      controller.quote.text.should == "I wanted to beat the heck out of the JV guys for that, except I wouldn't know what to do in a fistfight without a manual. ,"
+    end
+
   end
 
 end
